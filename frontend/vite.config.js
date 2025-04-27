86fc27e133b3f506186ccef9ea1fe308d9fbc75f
Aditi Kala <aditi25.kala@gmail.com>
2025-04-27 18:49:09 +0530

    hopefully

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import { loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, import.meta.env.VITE_API_URL, "");
+  const env = loadEnv(mode, process.cwd(), ""); // <-- fix here
+
   return {
     plugins: [react()],
     server: {
