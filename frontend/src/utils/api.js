6abb319185846ec81515e4941048b76f7e1b483a
Aditi Kala <aditi25.kala@gmail.com>
2025-04-27 17:37:26 +0530

    deploy stuff

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an axios instance with default config
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
